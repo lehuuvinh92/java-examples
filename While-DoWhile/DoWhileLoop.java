5228a1db37900a253f146705daa00736228c9d2b
Vinh Le Huu <lehuuvinh92@gmail.com>
2022-08-09 09:20:58 +0700

    Add files via upload

---
+++ While-DoWhile/DoWhileLoop.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gochocit.examples;
+
+/**
+ *
+ * @author LHVINH
+ */
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1, n = 5;
+        // do...while loop from 1 to 5
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+    }
+}
---
+++ While-DoWhile/WhileLoop.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gochocit.examples;
+
+/**
+ *
+ * @author LHVINH
+ */
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        // declare variables
+        int i = 1, n = 5;
+        // while loop from 1 to 5
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
